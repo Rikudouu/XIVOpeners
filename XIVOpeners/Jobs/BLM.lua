71f8ed1cf9958270337994c8ca2b78d2c216f30c
Rikudou <dboy740@gmail.com>
2019-08-10 15:01:11 -0700

    blm will often stall on thunder 3, need to figure out why

--- XIVOpeners/Jobs/BLM.lua
+++ XIVOpeners/Jobs/BLM.lua
@@ -183,7 +183,7 @@ end
 
 function xivopeners_blm.updateLastCast()
     if (xivopeners_blm.lastCastFromQueue) then
---        xivopeners.log(tostring(xivopeners_blm.lastcastid) .. ", " .. tostring(xivopeners_blm.lastcastid2) .. ", " .. tostring(Player.castinginfo.lastcastid) .. ", " .. tostring(Player.castinginfo.castingid))
+        xivopeners.log(tostring(xivopeners_blm.lastcastid) .. ", " .. tostring(xivopeners_blm.lastcastid2) .. ", " .. tostring(Player.castinginfo.lastcastid) .. ", " .. tostring(Player.castinginfo.castingid))
         if (xivopeners_blm.lastcastid == -1) then
             -- compare the real castid and see if it changed, if it did, update from -1
             if (xivopeners_blm.lastcastid2 ~= Player.castinginfo.castingid) then
