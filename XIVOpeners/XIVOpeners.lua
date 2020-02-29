c60fd2c08e25e52ec0af3671981cdda7c8196f96
Rikudou <dboy740@gmail.com>
2020-02-29 00:01:53 -0800

    version bump 2, fucking git didn't push

--- XIVOpeners/XIVOpeners.lua
+++ XIVOpeners/XIVOpeners.lua
@@ -5,7 +5,7 @@ xivopeners.ModuleSettingPath = xivopeners.LuaPath .. [[ffxivminion\xivopener\]]
 xivopeners.ModuleSettings = xivopeners.ModuleSettingPath .. [[Settings.lua]]
 xivopeners.PreviousSave,xivopeners.lastcheck = {},Now()
 
-xivopeners.version = semver(1,7,0)
+xivopeners.version = semver(1,7,1)
 
 xivopeners.GUI = {
     open = false,
