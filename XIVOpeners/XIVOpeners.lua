661acc5a7b8e55f5901b0315a563dbf19a5bb5e5
Rikudou <dboy740@gmail.com>
2020-03-28 00:16:28 -0700

    updated gnb openers

--- XIVOpeners/XIVOpeners.lua
+++ XIVOpeners/XIVOpeners.lua
@@ -5,7 +5,7 @@ xivopeners.ModuleSettingPath = xivopeners.LuaPath .. [[ffxivminion\xivopener\]]
 xivopeners.ModuleSettings = xivopeners.ModuleSettingPath .. [[Settings.lua]]
 xivopeners.PreviousSave,xivopeners.lastcheck = {},Now()
 
-xivopeners.version = semver(1,7,3)
+xivopeners.version = semver(1,7,4)
 
 xivopeners.GUI = {
     open = false,
