0041c6e8c67de8c8874fd125aba8da0ced604cb8
Rikudou <dboy740@gmail.com>
2020-06-21 11:30:16 -0700

    version bump

--- XIVOpeners/XIVOpeners.lua
+++ XIVOpeners/XIVOpeners.lua
@@ -5,7 +5,7 @@ xivopeners.ModuleSettingPath = xivopeners.LuaPath .. [[ffxivminion\xivopener\]]
 xivopeners.ModuleSettings = xivopeners.ModuleSettingPath .. [[Settings.lua]]
 xivopeners.PreviousSave,xivopeners.lastcheck = {},Now()
 
-xivopeners.version = semver(1,8,0,"WIP-R4")
+xivopeners.version = semver(1,8,0,"WIP-R5")
 
 xivopeners.GUI = {
     open = false,
